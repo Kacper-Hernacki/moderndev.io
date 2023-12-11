2a69501f6ba4a49441a16de76891e4c791234d44
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-12-11 17:04:17 +0100

    feat: pro link

--- components/layouts/main/items/upgradeBanner.tsx
+++ components/layouts/main/items/upgradeBanner.tsx
@@ -2,6 +2,7 @@
 
 import { useSubscriptionStore } from "@/store/store";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const UpgradeBanner = () => {
   const subscription = useSubscriptionStore((state) => state.subscription);
@@ -11,8 +12,8 @@ export const UpgradeBanner = () => {
   if (subscription === undefined || isPro) return null;
 
   return (
-    <div>
+    <Link href="/pro">
       <button className="btn btn-accent btn-sm">Upgrade to PRO</button>
-    </div>
+    </Link>
   );
 };
