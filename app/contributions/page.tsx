29ea749ab90643acc970a4e04aaadf3ac8cc1087
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-12-13 15:38:35 +0100

    feat(collaborators): improved building collab

--- app/contributions/page.tsx
+++ app/contributions/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-export async function fetchCollaborators() {
+async function fetchCollaborators() {
   const response = await fetch(
     'https://api.github.com/repos/Kacper-Hernacki/moderndev.io/collaborators',
     {
