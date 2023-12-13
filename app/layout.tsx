37eb5c75e089c4ac33d0fc0ef6e2c1b9d5e4175e
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-12-13 18:46:19 +0100

    feat: speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ import SnackbarComponentProvider from "@/components/providers/snackbar";
 import SubscriptionProvider from "@/components/providers/subscription";
 import NextTopLoader from 'nextjs-toploader';
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -47,6 +47,7 @@ export default async function RootLayout({
           </SnackbarComponentProvider>
         </SubscriptionProvider>
       </FirebaseAuthProvider>
+      <SpeedInsights />
       <Analytics />
       </body>
       </html>
