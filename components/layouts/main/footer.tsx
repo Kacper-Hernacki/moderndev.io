9dffff77229cfa98d28cdbca6fb408e79968fd63
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-12-13 13:37:46 +0100

    feat(version): version indicator in a footer

--- components/layouts/main/footer.tsx
+++ components/layouts/main/footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Github, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
 import Link from "next/link";
+import packageJson from "@/package.json";
 
+const appVersion = packageJson.version || "1.0.0";
 export const Footer = () => {
   return (
     <footer className="footer footer-center p-10 text-base-content rounded">
-      {/* Navigation links */}
       <nav className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
         <Link href={'/about'}>About</Link>
         <Link href={'/tags'}>Tags</Link>
@@ -21,7 +22,6 @@ export const Footer = () => {
         <Link href={'/cookies'}>Cookies</Link>
       </nav>
 
-      {/* Social media icons */}
       <nav>
         <div className="grid grid-flow-col gap-4">
           <Link target="_blank" href={'https://www.instagram.com/kacper.hernacki/'}>
@@ -42,10 +42,15 @@ export const Footer = () => {
         </div>
       </nav>
 
-      {/* Footer note and copyright */}
       <aside>
+        <p> Version {appVersion}</p>
         <nav>
-          Do you see bugs? Contribute <Link target="_blank" href="https://github.com/Kacper-Hernacki/moderndev.io"><span className="gradient-span">here</span></Link>
+          <p>
+            Do you see bugs? Contribute{" "}
+            <Link target="_blank" href="https://github.com/Kacper-Hernacki/moderndev.io">
+              <span className="gradient-span">here</span>
+            </Link>
+          </p>
         </nav>
         <p>Copyright © 2023 - All right reserved by moderndev.io</p>
       </aside>
