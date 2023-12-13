1b0be6369a7a32855725298a2d09c65f50b8da88
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-12-13 12:51:14 +0100

    feat(release): semantinc release & sentry setup

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5fc6c46a9a58027bf9ba4fe649300d4c@o4506384296247296.ingest.sentry.io/4506384301621248",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
